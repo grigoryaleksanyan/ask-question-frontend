6b6134b4fab26aadb88a1c4f40a5a08a03329ef7
Григорий Алексанян <grigoryaleksanyan@outlook.com>
2022-04-12 09:51:38 +0300

    feat(client): добавить навигацию по порталу
    Добавить навигацию в шапке и в боковом меню

---
+++ src/core/components/DrawerNavigation.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-list nav class="ma-0 pa-0">
+    <v-list-item v-for="item in navItems" :key="item.title" :to="item.link" link class="mb-0">
+      <v-list-item-icon>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-list-item-icon>
+
+      <v-list-item-content>
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script>
+export default {
+  name: 'DrawerNavigation',
+  props: {
+    navItems: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
--- src/core/layouts/DefaultLayout.vue
+++ src/core/layouts/DefaultLayout.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
-    <v-navigation-drawer v-model="drawer" app temporary dark> </v-navigation-drawer>
+    <v-navigation-drawer v-model="drawer" app temporary dark class="drawer-navigation">
+      <DrawerNavigation :nav-items="navItems" />
+    </v-navigation-drawer>
 
     <v-app-bar app dark>
-      <v-app-bar-nav-icon @click="toggleDrawer" />
-      <v-app-bar-title class="ss">Заголовок</v-app-bar-title>
+      <v-app-bar-nav-icon class="drawer-navigation-burger" @click="toggleDrawer" />
+      <v-app-bar-title>Заголовок</v-app-bar-title>
+      <v-spacer></v-spacer>
+      <HeaderNavigation :nav-items="navItems" class="header-navigation" />
     </v-app-bar>
 
     <v-main>
@@ -14,16 +18,51 @@
       </v-container>
     </v-main>
 
-    <v-footer app dark> @{{ year }} Grigory Aleksanyan </v-footer>
+    <v-footer app dark>
+      @{{ year }} Grigory Aleksanyan
+      <v-spacer></v-spacer>
+      <v-tooltip left>
+        <template #activator="{ on, attrs }">
+          <v-btn to="/feedback" dark icon v-bind="attrs" v-on="on">
+            <v-icon size="24px">mdi-email-open</v-icon>
+          </v-btn>
+        </template>
+        <span>Обратная связь по порталу</span>
+      </v-tooltip>
+    </v-footer>
   </div>
 </template>
 
 <script>
+import DrawerNavigation from '@/core/components/DrawerNavigation.vue';
+import HeaderNavigation from '@/core/components/HeaderNavigation.vue';
+
 export default {
   name: 'DefaultLayout',
+  components: {
+    DrawerNavigation,
+    HeaderNavigation,
+  },
   data() {
     return {
       drawer: false,
+      navItems: [
+        {
+          title: 'Главная',
+          icon: 'mdi-apps',
+          link: '/',
+        },
+        {
+          title: 'Все вопросы',
+          icon: 'mdi-comment-question-outline',
+          link: '/questions',
+        },
+        {
+          title: 'FAQ',
+          icon: 'mdi-account-question-outline',
+          link: '/faq',
+        },
+      ],
     };
   },
   computed: {
@@ -39,3 +78,17 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+@media (max-width: 600px) {
+  .header-navigation {
+    display: none;
+  }
+}
+@media (min-width: 600px) {
+  .drawer-navigation,
+  .drawer-navigation-burger {
+    display: none;
+  }
+}
+</style>
