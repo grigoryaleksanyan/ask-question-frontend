66b43aae9dcf682d04ac259f09b6b3c727a1beb2
Григорий Алексанян <grigoryaleksanyan@outlook.com>
2022-04-18 17:15:03 +0300

    feat(client): сверстать блок статуса вопроса

---
+++ src/core/constants/question-status-colors.js
@@ -0,0 +1,9 @@
+// Цветовые константы по статусам для вопросов
+
+export const Q_NEW_COLOR = '#1976d2';
+
+export const Q_FOCUS_COLOR = '#ff9800';
+
+export const Q_COMMENT_COLOR = '#428f4e';
+
+export const Q_ANSWERED_COLOR = '#9FB980';
