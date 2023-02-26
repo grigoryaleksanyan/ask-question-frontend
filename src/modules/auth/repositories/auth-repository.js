bfe409fc019fef7f20b781f8a5c5c80bdf8a0bf2
Григорий Алексанян <grigoryaleksanyan@outlook.com>
2023-02-26 19:02:57 +0300

    feat(client): добвил метод Logout в auth-repository

--- src/modules/auth/repositories/auth-repository.js
+++ src/modules/auth/repositories/auth-repository.js
@@ -1,13 +1,24 @@
 import httpClient from '@/core/plugins/http-client';
 
-const apiRoute = '/api/auth';
+const apiRoute = '/api/Auth';
 
-export default async function Login(authData) {
+export async function Login(authData) {
   const result = await httpClient
-    .post(`${apiRoute}/login`, authData)
+    .post(`${apiRoute}/Login`, authData)
     .then((response) => response.data)
     .catch((error) => {
       throw new Error('Ошибка авторизации', { cause: error });
+    });
+
+  return result;
+}
+
+export async function Logout() {
+  const result = await httpClient
+    .put(`${apiRoute}/Logout`)
+    .then((response) => response.data)
+    .catch((error) => {
+      throw new Error('Ошибка выхода', { cause: error });
     });
 
   return result;
