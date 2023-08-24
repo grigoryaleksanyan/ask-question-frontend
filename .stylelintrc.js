9a85b7498e6eeed2a4e078e4b7c4995f2b00ecc1
Григорий Алексанян <grigoryaleksanyan@outlook.com>
2023-08-24 19:33:27 +0300

    build: заменил расширение stylelint для сортировки стилей

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     'stylelint-config-html/vue',
     'stylelint-config-standard-scss',
     'stylelint-config-recommended-vue/scss',
-    'stylelint-config-rational-order',
+    'stylelint-config-idiomatic-order',
   ],
   plugins: ['stylelint-scss', 'stylelint-order'],
   files: ['./src/**/*.{vue,css,scss}'],
