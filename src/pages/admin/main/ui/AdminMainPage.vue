e01921a78a605be735bd45d6b3c15e785a686396
Григорий Алексанян <grigoryaleksanyan@outlook.com>
2026-05-31 01:40:07 +0300

    refactor(admin): remove unnecessary header from AdminMainPage template

--- src/pages/admin/main/ui/AdminMainPage.vue
+++ src/pages/admin/main/ui/AdminMainPage.vue
@@ -2,14 +2,6 @@
   <div
     style="max-width: 1200px"
     class="text-left p-5 mx-auto">
-    <div class="grid">
-      <div class="col-12">
-        <h1
-          class="typography__headline--small typography__headline--medium--sm">
-          Сводка
-        </h1>
-      </div>
-    </div>
     <DashboardWidget />
   </div>
 </template>
