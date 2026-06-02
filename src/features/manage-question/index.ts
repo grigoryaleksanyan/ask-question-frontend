353b33fd739c71d60a1f183fad89507becad0c37
Григорий Алексанян <grigoryaleksanyan@outlook.com>
2026-06-02 21:52:07 +0300

    feat(manage-question): add public API

---
+++ src/features/manage-question/index.ts
@@ -0,0 +1,3 @@
+export { default as QuestionStatusDropdown } from './ui/QuestionStatusDropdown.vue';
+export { default as QuestionCommentButton } from './ui/QuestionCommentButton.vue';
+export { default as QuestionBulkActions } from './ui/QuestionBulkActions.vue';
