41d17a245b93fa39814251ebe8d618ed3bc28644
Григорий Алексанян <grigoryaleksanyan@outlook.com>
2026-06-07 12:21:59 +0300

    chore: add vite-plugin-vue-devtools and update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { fileURLToPath, URL } from 'node:url';
 
 import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: [
       {
         find: '@',
-        replacement: fileURLToPath(new URL('./src', import.meta.url)),
+        replacement: fileURLToPath(new URL('src', import.meta.url)),
       },
     ],
   },
